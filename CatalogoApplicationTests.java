3e69f94da3f9d843c901db0dd8c36f4980d9e4bb
Shirley Huntress <shirley_naruto@yahoo.com.br>
2024-06-27 17:16:56 -0300

    Add files via upload
    Signed-off-by: Shirley Huntress <shirley_naruto@yahoo.com.br>

---
+++ CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literalura.catalogo.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
